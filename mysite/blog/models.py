6621258e7d37213f24172d5e569cbd9bde20cfdd
Kimhh <khh25864@naver.com>
2018-11-16 07:02:51 +0900

    add login and logout.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,3 +13,7 @@ class Post(models.Model):
         self.save()
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
